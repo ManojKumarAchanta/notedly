0a1c4b8af97671858f6bf8cb5f2b1f88fe0cee0f
ManojKumarAchanta <manojachanta7172@gmail.com>
2025-06-27 20:01:31 +0530

    ai

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -3,40 +3,14 @@ import { notesApi } from "./services/notesApi";
 import { authApi } from "./services/authApi";
 import authReducer from "./features/authSlice";
 import uiReducer from "./features/uiSlice";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage"; // defaults to localStorage
-import { combineReducers } from "@reduxjs/toolkit";
-
-const persistConfig = {
-  key: "root",
-  storage,
-  whitelist: ["ui"], // only persist ui slice
-};
-
-const rootReducer = combineReducers({
-  ui: uiReducer,
-  auth: authReducer,
-  [authApi.reducerPath]: authApi.reducer,
-  [notesApi.reducerPath]: notesApi.reducer,
-  // add other reducers here
-});
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: {
+    auth: authReducer,
+    ui: uiReducer,
+    [authApi.reducerPath]: authApi.reducer,
+    [notesApi.reducerPath]: notesApi.reducer,
+  },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [
-          "persist/PERSIST",
-          "persist/REHYDRATE",
-          "persist/REGISTER",
-        ],
-      },
-    })
-      .concat(authApi.middleware) // Add authApi middleware
-      .concat(notesApi.middleware), // Add notesApi middleware
+    getDefaultMiddleware().concat(authApi.middleware, notesApi.middleware),
 });
-
-export const persistor = persistStore(store);
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,18 +5,14 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from './components/theme-provider'
 import { Provider } from 'react-redux'
-import { persistor, store } from './app/store'
-import { PersistGate } from 'redux-persist/integration/react'
+import { store } from './app/store'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <Provider store={store}>
-          <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
-            <App />
-          </PersistGate>
-
+          <App />
         </Provider>
       </ThemeProvider>
     </BrowserRouter>
